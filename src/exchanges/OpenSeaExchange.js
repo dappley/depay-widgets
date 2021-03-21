20e11fea8e7d2b7b8e74299d718b442e66617e29
Sebastian Pape <s@pape.io>
2021-03-21 10:45:40 +0100

    fix: opensea orders is 0 here

--- src/exchanges/OpenSeaExchange.js
+++ src/exchanges/OpenSeaExchange.js
@@ -28,7 +28,7 @@ class OpenSeaExchange {
       if(completeRoute.nft){
         fetch(`https://api.opensea.io/wyvern/v1/orders?bundled=false&include_bundled=false&include_invalid=false&limit=5&offset=0&order_by=base_price&order_direction=desc&asset_contract_address=`+completeRoute.token.address+`&token_id=`+completeRoute.token.id).then(function(response){
           response.json().then(function(data) {
-            if(data.count < 0) {
+            if(data.count <= 0) {
               resolve(null);
             } else {
               resolve([
